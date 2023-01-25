acb8d3bcd34f15d7e3e46cf3d8bf7dede928ff24
patrykfussek <patryk.fussek@gmail.com>
2023-01-25 14:22:43 +0100

    card flip & match mechanism

--- src/App.js
+++ src/App.js
@@ -48,8 +48,7 @@ function App() {
         })
         resetTurn()
       } else {
-
-        resetTurn()
+        setTimeout(() => resetTurn(), 1000)
       }
 
     }
@@ -71,7 +70,12 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          <SingleCard 
+          key={card.id} 
+          card={card} 
+          handleChoice={handleChoice} 
+          flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
