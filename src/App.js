b6a6fabb15a99e6db5e0f53d99dfea0e6e14a6ff
patrykfussek <patryk.fussek@gmail.com>
2023-01-25 13:58:31 +0100

    match checking mechanism

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  {"src": "/img/helmet-1.png"},
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"}
-]
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 
 function App() {
-
-  const [cards, setCards] = useState([])
-  const [turns, setTurns] = useState(0)
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   //shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages] //spread syntax
-    .sort(() => Math.random() - 0.5) // some cards get sorted and some not
-    .map((card) => ({...card, id: Math.random()})) 
-      
-    setCards(shuffledCards)
-    setTurns(0)
-  }
+      .sort(() => Math.random() - 0.5) // some cards get sorted and some not
+      .map((card) => ({ ...card, id: Math.random() }));
 
-  console.log(cards, turns)
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  //handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  };
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  };
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
 
-      <div className='card-grid'>
-        {cards.map(card => (
-          <div className='card' key={card.id}>
-            <div> 
-              <img className='front' src={card.src} alt='card front' />
-              <img className='back' src='/img/cover.png' alt='card back' />
-            </div>
-          </div>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
